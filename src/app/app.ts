ad689952b746cfe08017038f2c81222396d15a70
sowie 43556 <114318263+sowiexsker894@users.noreply.github.com>
2025-06-10 17:01:58 -0500

    fit : componente inicio y login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html'
 })
-export class App {
-  protected title = 'ishowtime';
+export class AppComponent {
+  title = 'iShowTime';
 }
