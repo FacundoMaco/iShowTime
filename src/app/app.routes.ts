319a29e880e8edad5a443357cdfbc44a11b3c2dc
sowie 43556 <sihuinchamireya@gmail.com>
2025-06-23 17:42:50 -0500

    fix: Delete unnecessary files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,5 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
-import { Register } from './pages/register/register';
-import { Inicio } from './pages/inicio/inicio';
+
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-    { path: 'inicio', component: Inicio },
-    { path: 'login', component: Login },
-    { path: 'register', component: Register }
 ];
