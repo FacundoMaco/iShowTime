5c1c56515f1acd6c02e98c89ce5705a3a4b94b72
SPESNA862 <fabriziobussalleu@gmail.com>
2025-06-23 20:55:44 -0500

    feat: agrega página y ruta de Nosotros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Inicio } from './pages/inicio/inicio';
+import { Nosotros } from './pages/nosotros/nosotros';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'inicio', pathMatch: 'full' },
     { path: 'inicio', component: Inicio },
     { path: 'login', component: Login },
-    { path: 'register', component: Register }
+    { path: 'register', component: Register },
+    { path: 'nosotros', component: Nosotros }
 ];
